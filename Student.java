baa7569020602096aa08d3d372165413ae3a87c1
arasecuya <21103865@usc.edu.ph>
2022-09-16 17:56:14 -0700

    okay

---
+++ Student.java
@@ -0,0 +1,35 @@
+package secuya_salado_mangapuro;
+
+    @author 1styrGroupB
+
+    public class Student {
+        String name;
+        int age;
+        String gender;
+        String course;
+        
+        public void setAge(int newAge){
+            age = newAge;
+        }
+        public int getAge(){
+            return age;
+        }
+        public void setName(String newName){
+            name = newName;
+        }
+        public String getName(){
+            return name;
+        }
+        public void setGender(String newGender){
+            gender = newGender;
+        }
+        public String getGender(){
+            return gender;
+        }
+        public void setCourse(String newCourse){
+        course = newCourse;
+        }
+        public String getCourse(){
+            return course;
+        }
+}
