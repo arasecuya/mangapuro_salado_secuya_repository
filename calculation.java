5abb8ae98075f5f560e8284e4e85ae441211389f
kwengXSNA <112536564+kwengXSNA@users.noreply.github.com>
2022-09-16 21:55:49 -0700

    okay

--- calculation.java
+++ calculation.java
@@ -1,4 +1,4 @@
-  public int multiply(int num1, int num2){
-        result = num1*num2;
-        return result;
-    }
+public int multiply(int num1, int num2){
+    result = num1*num2;
+    return result;
+}
