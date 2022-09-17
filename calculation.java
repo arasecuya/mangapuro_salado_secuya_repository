39dfef5603b7447cfe10f0849a54437d2e8109fe
kwengXSNA <112536564+kwengXSNA@users.noreply.github.com>
2022-09-16 21:52:58 -0700

    ooo

--- calculation.java
+++ calculation.java
@@ -1,4 +1,4 @@
-public int substract(int num1, int num2){
-        result = num1-num2;
-        return result;
-}
+    public int substract(int num1, int num2){
+            result = num1-num2;
+            return result;
+    }
